08c120f11463765bd22348b0cf4a487a61b83307
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-04-23 18:16:58 -0300

    hola

--- src/topologia.py
+++ src/topologia.py
@@ -1,13 +1,12 @@
-import argparse
 from mininet.topo import Topo
 from mininet.link import TCLink
 
-DEFAULT_HOST_AMOUNT = 1
+MIN_HOST_AMOUNT = 1
 DEFAULT_LINK_LOSS = 10
 
 
 class CustomTopo(Topo):
-    def __init__(self, size, loss):
+    def __init__(self, size=MIN_HOST_AMOUNT, loss=DEFAULT_LINK_LOSS):
         Topo.__init__(self, size)
         server = self.addHost("server")
         switch = self.addSwitch("switch")
@@ -19,14 +18,14 @@ class CustomTopo(Topo):
             self.addLink(host, switch, cls=TCLink, loss=loss)
 
 
-parser = argparse.ArgumentParser(
-    prog="Topology", description="Amount of hosts for the server topology"
-)
+# parser = argparse.ArgumentParser(
+#     prog="Topology", description="Amount of hosts for the server topology"
+# )
 
-parser.add_argument("-s", "--size", default=DEFAULT_HOST_AMOUNT, type=int, nargs=1)
-parser.add_argument("-l", "--loss", default=DEFAULT_LINK_LOSS, type=int, nargs=1)
-args = parser.parse_args()
-size = args.size
-loss = args.loss
+# parser.add_argument("-s", "--size", default=DEFAULT_HOST_AMOUNT, type=int, nargs=1)
+# parser.add_argument("-l", "--loss", default=DEFAULT_LINK_LOSS, type=int, nargs=1)
+# args = parser.parse_args()
+# size = args.size
+# loss = args.loss
 
-topos = {"customTopo": (lambda: CustomTopo(size, loss))}
+topos = {"customTopo": (lambda: CustomTopo())}
