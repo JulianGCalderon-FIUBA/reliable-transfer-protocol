9e5becc53265d35f7701fa417d593cf6d6dd1b56
Julian Gonzalez Calderon <gonzalezcalderonjulian@gmail.com>
2023-04-24 16:55:06 -0300

    polish segmenter

--- src/lib/connection.py
+++ src/lib/connection.py
@@ -4,7 +4,7 @@ Define la interfaz para la implementacion de conexiones de distinto tipo
 
 from abc import ABC
 from lib.constants import DATASIZE
-from lib.segmentation import Segmenter
+from lib.segmentation import Desegmenter, Segmenter
 from lib.packet import (
     DataFPacket,
 )
@@ -17,22 +17,14 @@ from lib.transport.transport import (
 class ConnectionRFTP(ABC):
     def __init__(self, socket: ReliableTransportClient):
         self.socket = socket
-        self.segmenter = Segmenter()
-
-    def close(self):
-        """
-        Cierra el socket
-        """
-
-        pass
 
     def send_file(self, file_path: str):
         """
         Envia los datos definidos en data a traves de la conexion
         """
 
-        self.segmenter.segment(file_path)
-        for packet in self.segmenter:
+        segmenter = Segmenter(file_path)
+        for packet in segmenter:
             self.socket.send(packet.encode())
 
     def receive_file(self, file_path: str):
@@ -40,14 +32,14 @@ class ConnectionRFTP(ABC):
 
         packet = DataFPacket.decode_as_data(packet)
 
-        self.segmenter.desegment(file_path)
+        desegmenter = Desegmenter(file_path)
 
-        self.segmenter.add_segment(packet)
+        desegmenter.add_segment(packet)
         while packet.length >= DATASIZE:
             packet = self.socket.recv()
 
             packet = DataFPacket.decode_as_data(packet)
 
-            self.segmenter.add_segment(packet)
+            desegmenter.add_segment(packet)
 
-        self.segmenter.close()
+        desegmenter.close()
--- src/lib/segmentation.py
+++ src/lib/segmentation.py
@@ -3,30 +3,44 @@ from lib.packet import DataFPacket
 
 
 class Segmenter:
-    def __init__(self):
-        self.buffer = None
-        self.bytes = bytes()
+    """
+    Segments a file into DataFPackets on demand
 
-    def segment(self, file_path: str):
-        self.buffer = open(file_path, "rb")
+    This class is lazy. A packet is extracted
+    from the file on each iteration
+    """
 
-    def desegment(self, file_path: str):
-        self.buffer = open(file_path, "wb")
-
-    def add_segment(self, data: DataFPacket):
-        self.buffer.write(data.data)  # type: ignore
-
-    def close(self):
-        self.buffer.close()  # type: ignore
+    def __init__(self, file_path: str):
+        self.file = open(file_path, "rb")
 
     def __iter__(self):
         return self
 
-    def __next__(self) -> DataFPacket:
-        data = self.buffer.read(DATASIZE)  # type: ignore
+    def __next__(self):
+        data = self.file.read(DATASIZE)
 
         if len(data) <= 0:
-            self.close()
+            self.file.close()
             raise StopIteration
 
         return DataFPacket(len(data), data)
+
+
+class Desegmenter:
+    """
+    Constructs a file from DataFPackets"""
+
+    def __init__(self, file_path: str):
+        self.file = open(file_path, "wb")
+
+    def add_segment(self, data: DataFPacket):
+        """
+        Adds a segment to the file"""
+
+        self.file.write(data.data)
+
+    def close(self):
+        """
+        Closes the file"""
+
+        self.file.close()
