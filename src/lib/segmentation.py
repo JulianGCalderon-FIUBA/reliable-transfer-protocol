8b038a463981aca8976946256df753881682a4ba
Fran <franciscopere987@gmail.com>
2023-04-23 23:17:11 -0300

    Cambio segmenter para que ponga como numero de bloque del paquete data el tamaño del paquete

--- src/lib/segmentation.py
+++ src/lib/segmentation.py
@@ -1,21 +1,20 @@
 from lib.constants import DATASIZE
-from lib.exceptions import InvalidPacket
 from lib.packet import DataFPacket, TransportPacket
 
 
 class Segmenter:
     def __init__(self):
         self.segments = []
-        self.expected_segment = 1
 
     def segment(self, data: bytes):
         while len(data) > 0:
-            packet = DataFPacket(self.expected_segment, data[:DATASIZE])
+            packet_data = data[:DATASIZE]
+            packet = DataFPacket(len(packet_data), packet_data)
             data = data[DATASIZE:]
             self.add_segment(packet)
 
         if len(self.segments[-1].data) == DATASIZE:
-            self.segments.append(DataFPacket(self.expected_segment, bytes()))
+            self.segments.append(DataFPacket(0, bytes()))
 
     def desegment(self) -> bytes:
         segment_bytes = bytes()
@@ -25,13 +24,7 @@ class Segmenter:
         return segment_bytes
 
     def add_segment(self, data: DataFPacket):
-        if data.block != self.expected_segment:
-            raise InvalidPacket()
         self.segments.append(data)
-        self.advance_seq_number()
-
-    def advance_seq_number(self):
-        self.expected_segment += 1
 
     def __iter__(self):
         return self
